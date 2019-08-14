a266ec59e0e9a6b71c182bd01655162e54f45a64
ThCrombie <43470407+ThCrombie@users.noreply.github.com>
2019-08-14 14:44:24 +0100

    Working on TTS to tell the time
    Clock_Shield seems to have trouble working with the TTS Shield - Hours and Minutes are always reported as zero.

--- main/main.ino
+++ main/main.ino
@@ -1,16 +1,64 @@
-// including the 1sheeld library
+#define CUSTOM_SETTINGS
+#define INCLUDE_TEXT_TO_SPEECH_SHIELD
+#define INCLUDE_VOICE_RECOGNIZER_SHIELD
+#define INCLUDE_CLOCK_SHIELD
+
 #include <OneSheeld.h>
-#define INCLUDE_MIC_SHIELD // for voice controls
 
+const char firstCommand[]="what is the time";
+const char secondCommand[]="weather";
+const char thirdCommand[]="turn on";
 
-void setup() {
+char b[12];
+char c[12];
+int hour, minute;
+String stringHour, stringMinute;
+
+int ledPin = 13;
+
+void setup()
+{
   OneSheeld.begin();
-
-  
-
+  /* Set the LED pin as output. */
+  pinMode(ledPin,OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(){
+  hour = Clock.getHours();
+  minute = Clock.getMinutes();
+  stringHour = String(hour);
+  stringMinute = String(minute);
+  
+  /* Always check if a new voice command received. */ 
+  if(VoiceRecognition.isNewCommandReceived()){
+    /* Check if the voice command is the desired one. */
+    if(!strcmp(firstCommand,VoiceRecognition.getLastCommand())){
+      TextToSpeech.say("The time is...");
+      printf("%d hour %d minute/n", &stringHour, &stringMinute);
+      delay(500);
+      TextToSpeech.say(stringHour);
+      delay(300);
+      TextToSpeech.say(stringMinute);
+      TextToSpeech.say(" P M");
+     }
+     /* Check if the voice command is the desired one. */
+     else if(!strcmp(secondCommand,VoiceRecognition.getLastCommand()))
+     {
+       /* 1Sheeld responds using text-to-speech. */
+       TextToSpeech.say("the weather is pretty good sir");
+     }
+     /* Check if the voice command is the desired one. */
+     else if(!strcmp(thirdCommand,VoiceRecognition.getLastCommand()))
+     {
+       digitalWrite(ledPin,HIGH);
+       /* 1Sheeld responds using text-to-speech. */
+       TextToSpeech.say("led turned on");
+     }
+     else
+     {
+       digitalWrite(ledPin,LOW);
+       delay(1000);
+       TextToSpeech.say("I do not understand. Could you repeat that?");
+     }
+   }
 }
